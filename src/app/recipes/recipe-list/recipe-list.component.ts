759fce714e9cd0e29b13a2a122e47e97b3f8061e
Kleverson Kenji <kleversonkenji@gmail.com>
2022-06-20 19:48:14 -0300

    outputing a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
-export class RecipeListComponent {}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
