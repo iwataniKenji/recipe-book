496578e3c83fbc89f63277bef721ba0861592276
Kleverson Kenji <kleversonkenji@gmail.com>
2022-06-25 21:14:41 -0300

    passing recipe data with property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
 })
-export class RecipeItemComponent {}
+export class RecipeItemComponent {
+  @Input() recipe: Recipe;
+}
