fd534001a2721be15e33a7e1080a68059ea3b350
Kleverson Kenji <kleversonkenji@gmail.com>
2022-06-25 21:06:59 -0300

    organizing, commenting and reviewing the code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,9 +5,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  // output -> filho envia evento para pai
   @Output() featureSelected = new EventEmitter<string>();
 
   onSelect(feature: string) {
+    // featureSelected recebe valor da string e envia para o pai
     this.featureSelected.emit(feature);
   }
 }
