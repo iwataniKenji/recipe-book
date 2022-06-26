8f5a56bd85c894ed1016c6fc8d2f83375f64cc75
Kleverson Kenji <kleversonkenji@gmail.com>
2022-06-26 13:28:46 -0300

    toggle functionality fix

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -10,18 +10,17 @@ import {
 })
 export class DropdownDirective {
   // HostBinding -> muda class na condição
-  // @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.open') isOpen = false;
 
   // HostListener -> cria evento a ser ouvido e um método para manipulá-lo
-  // @HostListener('click') toggleOpen() {
-  //   this.isOpen = !this.isOpen;
-  // }
+  @HostListener('click') click() {
+    this.isOpen = !this.isOpen;
+  }
 
   // fechar ao clicar fora do toggle
-  @HostBinding('class.open') isOpen = false;
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-    this.isOpen = !this.elRef.nativeElement.contains(event.target)
-      ? !this.isOpen
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? this.isOpen
       : false;
   }
 
