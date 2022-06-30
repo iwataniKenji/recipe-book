721b10cdaa7fefa2f0516d6d0b7d392b5e380f0d
Kleverson Kenji <kleversonkenji@gmail.com>
2022-06-29 21:47:28 -0300

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
       'This is a test',
-      'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'
+      'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Fries', 20)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is a test',
-      'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'
+      'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Chicken', 1)]
     ),
   ];
 
